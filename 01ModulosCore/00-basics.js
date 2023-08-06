2e7e4fcd271c631f79d62f59764e3562841246d3
Matias <matute_11@hotmail.com.ar>
2023-08-06 00:00:23 -0300

    nociones basicas

---
+++ 01ModulosCore/00-basics.js
@@ -0,0 +1,28 @@
+//Buenas Practicas
+/*
+Organizacion de Codigo JS
+    1-librerias/modulos
+    2-constantes
+    3-objetos/variables
+    4-funciones
+    5-eventos
+    6-ejecuciones
+    
+Usar camelCase
+    ej: esAlgoAsiElCamelCase
+
+Usar comillas simples
+
+*/
+'use strict' //Modo estricto de JavaScript
+
+//HOLA MUNDO
+
+console.log('Hola Mundo desde Node.JS');
+
+console.log(2+5);
+
+//console.log(Window); muestra las funciones y metodos de Window en Chrome
+
+console.log(global); //Similar a Window pero funciona en la Powershell
+
