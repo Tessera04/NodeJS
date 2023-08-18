3983e4a87bc59025f5ff4798035e1fad41bc8083
Matias <matute_11@hotmail.com.ar>
2023-08-18 00:21:37 -0300

    Agregando type:module y probando los cambios

---
+++ 01ModulosCore/03_NPM/calculadora.js
@@ -0,0 +1,14 @@
+const sumar = (a, b) => a + b,
+      restar = (a ,b) => a - b,
+      multiplicar = (a, b) => a * b,
+      dividir = (a, b) => a / b,
+      modulo = (a, b) => a % b;
+
+export const calculadora = {
+        sumar, /*Esto es una clave:valor, pero al tener el mismo nombre no es necesario hacerlo con ese formato. sirve solo en js*/
+        restar,
+        multiplicar,
+        dividir,
+        modulo
+      }
+
